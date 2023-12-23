d2dd641a0e1d4a95721372117dbfa43fbf33daa6
k890m <94244306+k890m@users.noreply.github.com>
2023-12-23 12:19:29 -0500

    Different Raindrop asset
    Changed Raindrop assets and added create_blackdrops function. Added black raindrops so that they randomly fall from the top of the screen.

--- main.py
+++ main.py
@@ -18,22 +18,24 @@ PLANT = pygame.transform.scale(PLANT_IMG, (PLANT_WIDTH, PLANT_HEIGHT))
 
 
 RAINDROP_IMG = pygame.image.load(os.path.join('Assets', 'raindrop.png'))
-RAINDROP_WIDTH, RAINDROP_HEIGHT = 48, 32
-
+RAINDROP_WIDTH, RAINDROP_HEIGHT = 24, 36
+BLACKDROP_IMG = pygame.image.load(os.path.join('Assets', 'blackdrop.png'))
 
 BACKGROUND_IMG = pygame.image.load(os.path.join('Assets', 'back.jpg'))
 BACKGROUND_MAIN = pygame.transform.scale(BACKGROUND_IMG, (WIDTH, HEIGHT))
 
 
-
 # Start window
-def draw_window(plant, raindrops):
+def draw_window(plant, raindrops, blackdrops):
     WIN.blit(BACKGROUND_MAIN, (0,0))
     
     WIN.blit(PLANT, (plant.x, plant.y))
     
     for raindrop in raindrops:
         WIN.blit(raindrop['image'], (raindrop['x'], raindrop['y']))
+        
+    for drop in blackdrops:
+        WIN.blit(drop['image'], (drop['x'], drop['y']))
     
     pygame.display.update()
     
@@ -45,10 +47,19 @@ def create_raindrops():
         'y': -RAINDROP_HEIGHT
     }
 
+#Make black raindrops NOT DONE YET
+def create_blackdrops():
+    return {
+        'image': pygame.transform.scale(BLACKDROP_IMG, (RAINDROP_WIDTH, RAINDROP_HEIGHT)),
+        'x': random.randint(0, WIDTH - RAINDROP_WIDTH),
+        'y': -RAINDROP_HEIGHT
+    }
+
 
 def main():
     plant = pygame.Rect(100, 600, PLANT_WIDTH, PLANT_HEIGHT)
     raindrops = []
+    blackdrops = []
 
     clock = pygame.time.Clock()
     run = True 
@@ -69,14 +80,21 @@ def main():
         #New Raindrops
         if random.random() < .02:
             raindrops.append(create_raindrops())
+             
+        if random.random() < .02:
+            blackdrops.append(create_blackdrops())
             
         for raindrop in raindrops:
             raindrop['y'] += VEL
             
+        for drop in blackdrops:
+            drop['y'] += VEL
+            
         #clear raindrops afterwords
         raindrops = [raindrop for raindrop in raindrops if raindrop['y'] < HEIGHT]
+        blackdrops = [drop for drop in blackdrops if drop['y'] < HEIGHT]
 
-        draw_window(plant, raindrops)
+        draw_window(plant, raindrops, blackdrops)
 
     pygame.quit()
 
